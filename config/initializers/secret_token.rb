dabb8bdaa108ede9e0d1e78dd0ff606dbb87f24a
Gk <gk@parish-philp.com>
2010-11-16 15:29:32 -0800

    - Base Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seedz::Application.config.secret_token = 'fedb1c21ba6a167cb22f501c7f1df573982dd7be1dfec341d68c78bf00b1803d12e944a8f5a591881e47e46695ee3f10cc4977ea12afeb32a1fd95db701fb22f'
