10ce320a12f6b97e2bf7bb7423d22f0dd62beb2f
Tay Nguyen <tay.x.nguyen@gmail.com>
2011-02-01 20:06:17 -0800

    Sitemap generator configuration

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "http://thecherrytree.com"
 
 SitemapGenerator::Sitemap.add_links do |sitemap|
   # Put links creation logic here.
