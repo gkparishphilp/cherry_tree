c3a11eabce167ba79b472ddb761b880be2adb1b5
Gk <gk@Gks-MacBook-Pro.local>
2011-06-16 11:36:41 -0700

    - cosmetic tweaks

--- app/models/quiz_assignment.rb
+++ app/models/quiz_assignment.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+# Schema version: 20110420185439
+#
+# Table name: quiz_assignments
+#
+#  id          :integer(4)      not null, primary key
+#  user_id     :integer(4)
+#  quiz_id     :integer(4)
+#  creator_id  :integer(4)
+#  point_value :integer(4)      default(1)
+#  status      :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 
 class QuizAssignment < ActiveRecord::Base
 	belongs_to	:user
