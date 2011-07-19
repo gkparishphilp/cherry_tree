df7da6d62000df1c91ae58406c9de77fa7eb485c
tay <tay.x.nguyen@gmail.com>
2011-07-19 15:00:04 -0700

    delay

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -124,7 +124,7 @@ $(document).ready(function(){
 			var url = '/deactivate/award/assignment/' + $(this).attr('assignment') + '/for/' + $(this).attr('child');
 		}
 		$.get( url );
-		$.delay( 2000 );
+		$(this).delay( 2000 );
 		location.reload(true);
 	});
 	
