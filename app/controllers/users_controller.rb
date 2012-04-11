cd477963f8ad82a9a0fa153660b8e1821c68d8c6
Tay <tay.x.nguyen@gmail.com>
2012-04-11 14:45:33 -0700

    bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 			@relevant_assignment, @relevant_assignment_parent, @num_assignments = @current_user.most_relevant
 			@lesson_assignments= @child.lesson_assignments
 			
-			@active_assignments = @current_user.objective_assignments.active - @current_user.objective_assignments.checked_in
+			@active_assignments = @current_user.objective_assignments.active - @current_user.objective_assignments.map{ |assignment| assignment if assignment.checkin_in_last?(Time.now.beginning_of_day)}
 			@checked_in_assignments = @current_user.objective_assignments.checked_in
 			if @relevant_assignment_parent.is_a?( Lesson )
 				@viewing = LessonViewing.find_or_initialize_by_user_id_and_lesson_id( @current_user.id, @relevant_assignment_parent.id )
