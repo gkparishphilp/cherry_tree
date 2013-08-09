38de17c74783bc2611b15c695a27642d6922d1e5
Tay <tay.x.nguyen@gmail.com>
2013-08-09 08:13:13 -0700

    Disable contact emailer to stop spam

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -39,7 +39,7 @@ class ContactsController < ApplicationController
 		
 		if user.contacts << @contact
 			pop_flash "Thank you for your message"
-			UserMailer.send_contact( @user, @contact ).deliver
+			#UserMailer.send_contact( @user, @contact ).deliver
 		else
 			pop_flash "There was a problem with your message", :error, @contact
 			redirect_to :back
